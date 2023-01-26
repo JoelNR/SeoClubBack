3d9df5bf410a886b51d5830baf0610d4ac15d861
Joel Navarro Rivero <jnr_navarro_rivero@hotmail.com>
2023-01-26 17:35:05 +0000

    Update NewsSeeder.php

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -18,15 +18,17 @@ class NewsSeeder extends Seeder
         
         News::create([
             'title'=> 'Arqueros canarios en el Campeonato de España IBERDROLA al Aire Libre 2021-2022' ,
-            'content' => 'Arqueros de toda Canarias han competido en el Campeonato de España IBERDROLA al Aire Libre 2021-2022. Nos encanta ver tanta participación Canaria compitiendo fuera a nivel nacional. Dentro de los resultados obtenidos tenemos a Alberto López Pérez y Francisca Teresa Amador que consiguen plata en equipo tradicional mixto. Jorge Jose López, Jorge Julio Pérez y Nelson Eduardo Torres, bronce en equipo masculino Compuesto. Nelson Eduardo Torres, bronce en arco compuesto individual. Y por último, Carlos Luis Pey, plata Recurvo 50+. Un excelente resultado de la expedición Canaria, felicidades a todos los participantes. Agradecer el apoyo de la Federación Canaria para lograr la participación.',
+            'content' => 'Arqueros de toda Canarias han competido en el Campeonato de España IBERDROLA al Aire Libre 2021-2022. Nos encanta ver a tantos arqueros canarios compitiendo fuera a nivel nacional. 
+Dentro de los resultados obtenidos, tenemos a Alberto López Pérez y Francisca Teresa Amador que consiguen plata en equipo tradicional mixto. Jorge Jose López, Jorge Julio Pérez y Nelson Eduardo Torres, bronce en equipo masculino Compuesto. Nelson Eduardo Torres, bronce en arco compuesto individual. Y por último, Carlos Luis Pey, plata Recurvo 50+. 
+Un excelente resultado de la expedición Canaria, felicidades a todos los participantes. Agradecer el apoyo de la Federación Canaria para lograr la participación.',
             'date' => '2022-09-01',
             'image' => '/assets/img/competicion.jpeg'
         ]);
         News::create([
             'title'=> 'El equipo del club deportivo SEO consigue el quinto puesto' ,
             'content' => 'Nuestro equipo másculino recurvo ha conseguido el quinto puesto en la Fase Final Liga Nacional RFETA de Clubes. 
-            El primer día clásificaron en cuarto puesto al derrotar al club Asirio y en el domingo, después de perder la primera clásificatoria contra el club Arqueros de Valdemorillo, ganaron sus siguientes encuentros para quedar al final en un histórico quinto puesto.
-            ¡Enhorabuena a todo el equipo! Más que merecido premio después de la temporada que llevamos y el esfuerzo día a día.',
+El primer día clásificaron en cuarto puesto al derrotar al club Asirio y en el domingo, después de perder la primera clásificatoria contra el club Arqueros de Valdemorillo, ganaron sus siguientes encuentros para quedar al final en un histórico quinto puesto.
+¡Enhorabuena a todo el equipo! Más que merecido premio después de la temporada que llevamos y el esfuerzo día a día.',
             'date' => '2022-07-10',
             'image' => '/assets/img/equipo.jpg'
         ]);
@@ -38,7 +40,8 @@ class NewsSeeder extends Seeder
         ]);
         News::create([
             'title'=> '¡Nuevo arquero a 70 metros!' ,
-            'content' => 'El pasado mes, nuestro arquero Alexander Fleitas Hernández, realizó la prueba de ascenso para los 70 metros y la completó con éxito, uniendose al grupo de ocho arqueros que dispone el club en este nivel. ¡Felicidades a Alex, a disfrutar esos 70m!',
+            'content' => 'El pasado mes, nuestro arquero Alexander Fleitas Hernández, realizó la prueba de ascenso para los 70 metros y la completó con éxito, uniendose al grupo de ocho arqueros que dispone el club en este nivel. 
+¡Felicidades a Alex, a disfrutar esos 70m!',
             'date' => '2022-12-20',
             'image' => '/assets/img/ale-nivel.jpeg'
         ]);
